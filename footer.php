8af1fd3d1bb39e2ebddde9836db475ce1e0d1fef
sanjay <sanjay.bagtharia@gmail.com>
2016-10-23 22:41:44 +0200

    first upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">Copyright &copy; www.sanjaybagtharia.com 2016</span>
+                </div>
+                <div class="col-md-8">
+                    <ul class="list-inline social-buttons">
+                        
+                            <li><a href="http://www.twitter.com/"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li><a href="http://www.youtube.com"><i class="fa fa-youtube"></i></a>
+                            </li>
+                            <li><a href="http://www.instagram.com"><i class="fa fa-instagram"></i></a>
+                            </li>
+                            <li><a href="http://www.github.com"><i class="fa fa-github"></i></a>
+                            </li>
+                    </ul>
+                </div>
+                <!--<div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li><a href="#">Privacy Policy</a>
+                        </li>
+                        <li><a href="#">Terms of Use</a>
+                        </li>
+                    </ul>
+                </div>-->
+            </div>
+        </div>
+    </footer>
---
+++ modal.php
@@ -0,0 +1,109 @@
+
+<!-- Use the modals below to showcase details about your portfolio projects! -->
+
+    <!-- Portfolio Modal 1 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>My Own Site</h2>
+                                <p class="item-intro text-muted">My new site about myself!</p>
+                                <img class="img-responsive img-centered" src="img/portfolio/sbwebsite-preview.png" alt="">
+                                <p>This is my First project which I designed and built while attending A full stack web development course with Data4You </p>
+                                <p>
+                                    <strong>Want to able to design and build website?</strong>Then you can visit <a href="http://www.codingbootcamp.cz/">Data4YOU</a>.</p>
+                                <ul class="list-inline">
+                                    <li>Date: October 2016</li>
+                                    <li>Client: Myself</li>
+                                    <li>Category: Design & build</li>
+                                </ul>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 2 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>My Own Site</h2>
+                                <p class="item-intro text-muted">My new site about myself!</p>
+                                <img class="img-responsive img-centered" src="img/portfolio/sbwebsite-preview.png" alt="">
+                                <p>This is my First project which I designed and built while attending A full stack web development course with Data4You </p>
+                                <p>
+                                    <strong>Want to able to design and build website?</strong>Then you can visit <a href="http://www.codingbootcamp.cz/">Data4YOU</a>.</p>
+                                <ul class="list-inline">
+                                    <li>Date: October 2016</li>
+                                    <li>Client: Myself</li>
+                                    <li>Category: Design & build</li>
+                                </ul>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 3 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>My Own Site</h2>
+                                <p class="item-intro text-muted">My new site about myself!</p>
+                                <img class="img-responsive img-centered" src="img/portfolio/sbwebsite-preview.png" alt="">
+                                <p>This is my First project which I designed and built while attending A full stack web development course with Data4You </p>
+                                <p>
+                                    <strong>Want to able to design and build website?</strong>Then you can visit <a href="http://www.codingbootcamp.cz/">Data4YOU</a>.</p>
+                                <ul class="list-inline">
+                                    <li>Date: October 2016</li>
+                                    <li>Client: Myself</li>
+                                    <li>Category: Design & build</li>
+                                </ul>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
